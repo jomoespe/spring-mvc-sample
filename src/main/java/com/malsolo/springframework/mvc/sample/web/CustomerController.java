52ed95e660c03c00fde69a59b8a7099c74579cf6
Javier Beneito Barquero <jbbarquero@gmail.com>
2014-09-05 14:21:01 +0200

    Eliminate the License Headers from the Java files

--- src/main/java/com/malsolo/springframework/mvc/sample/web/CustomerController.java
+++ src/main/java/com/malsolo/springframework/mvc/sample/web/CustomerController.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.malsolo.springframework.mvc.sample.web;
 
 import com.malsolo.springframework.mvc.sample.domain.Customer;
@@ -14,7 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
--- src/main/java/com/malsolo/springframework/mvc/sample/web/WelcomeController.java
+++ src/main/java/com/malsolo/springframework/mvc/sample/web/WelcomeController.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.malsolo.springframework.mvc.sample.web;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 import java.util.Date;
 import org.springframework.stereotype.Controller;
